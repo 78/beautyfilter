de2d0e70a4ec5602ea28dc7aa3f791ada6b98a0c
Terrence <xx@fb.cc>
2020-03-27 08:27:30 +0800

    structure

--- core/WebGL.js
+++ core/WebGL.js
@@ -4,8 +4,6 @@
 // @xx
 // 
 
-import glUtils from './webgl-utils.js'
-
 
 export default class WebGL {
   constructor(canvas) {
@@ -14,19 +12,65 @@ export default class WebGL {
     if(!this.gl) {
       throw new Error('WebGL not supported.')
     }
-    this.gl.viewport(0, 0, canvas.width, canvas.height)
-    console.log('canvas size', canvas.width, canvas.height)
     this.programs = {}
     this.textures = []
     this.framebuffers = []
   }
+
+  createProgramFromSources(gl, sources) {
+    // Create the shader object
+    const vs = gl.createShader(gl.VERTEX_SHADER)
+    const fs = gl.createShader(gl.FRAGMENT_SHADER)
+
+    // Load the shader source
+    gl.shaderSource(vs, sources[0])
+    gl.shaderSource(fs, sources[1])
+    // Compile the shader
+    gl.compileShader(vs)
+    gl.compileShader(fs)
+
+    // Check the compile status
+    if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) {
+      // Something went wrong during compilation; get the error
+      const lastError = gl.getShaderInfoLog(vs)
+      errFn('*** Error compiling shader \'' + vs + '\':' + lastError)
+      gl.deleteShader(vs)
+      return null
+    }
+    // Check the compile status
+    if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
+      // Something went wrong during compilation; get the error
+      const lastError = gl.getShaderInfoLog(fs)
+      errFn('*** Error compiling shader \'' + fs + '\':' + lastError)
+      gl.deleteShader(fs)
+      return null
+    }
+
+    const program = gl.createProgram()
+    gl.attachShader(program, vs)
+    gl.attachShader(program, fs)
+    gl.linkProgram(program)
+
+    // Check the link status
+    const linked = gl.getProgramParameter(program, gl.LINK_STATUS)
+    if (!linked) {
+        // something went wrong with the link
+        const lastError = gl.getProgramInfoLog(program)
+        errFn('Error in program linking:' + lastError)
+
+        gl.deleteProgram(program)
+        return null
+    }
+    return program
+  }
+
 
   createProgram(name, vertexShader, fragmentShader, uniforms) {
     console.log('[WebGL] Creating program', name)
     const gl = this.gl
     const p = {}
 
-    p.program = glUtils.createProgramFromSources(gl, [vertexShader, fragmentShader])
+    p.program = this.createProgramFromSources(gl, [vertexShader, fragmentShader])
     p.positionLocation = gl.getAttribLocation(p.program, 'a_position')
     p.flipYLocation = gl.getUniformLocation(p.program, "u_flipY")
 
@@ -96,6 +140,7 @@ export default class WebGL {
     }
 
     const gl = this.gl
+    gl.viewport(0, 0, this.canvas.width, this.canvas.height)
     //gl.clearColor(0, 0, 0, 0)
     //gl.clear(gl.COLOR_BUFFER_BIT)
 
